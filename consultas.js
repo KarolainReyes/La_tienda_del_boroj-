12b87d1f358a087788b2328ae95af16022195d23
KarolainReyes <reyeskarol0412@gmail.com>
2025-08-21 10:26:21 -0500

    feat: :sparkles: consultas.js con operadores sobre los arrays
    operadores sobre arrays
    
    by@KarolainReyes

--- consultas.js
+++ consultas.js
@@ -11,3 +11,18 @@
     //Encontrar clientes cuyo nombre tenga la letra "z" (insensible a mayúsculas/minúsculas).
 
     db.clientes.find({nombre: {$regex: 'z', $options: 'im'}})
+
+//Operadores en consultas sobre arrays
+
+    //Buscar clientes que tengan `"natural"` en sus preferencias.
+
+    db.clientes.find({preferencias: {$in: ['natural']}})
+
+    //Encontrar productos que tengan al menos los tags `"natural"` y `"orgánico"` (usa `$all`).
+
+    db.productos.find({tags: {$all: ['natural', 'organico']}})
+
+    //Listar productos que tienen **más de un tag** (`$size`).
+
+    db.productos.find({tags:{$size: 2}})
+
