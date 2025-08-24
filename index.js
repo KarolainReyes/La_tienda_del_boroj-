8e92f091fa9fab8c9443aeeb0e1051702b23b263
Andre07G <eleal087@gmail.com>
2025-08-23 23:49:24 -0500

    feat: :sparkles: Creacion de archivo con indices
    Archivo que inserta indices en las colecciones añadido
    
    por @Andres07g

---
+++ index.js
@@ -0,0 +1,19 @@
+//Creacion de indices 
+
+// 1. Crear un índice en el campo `nombre` de `productos` para mejorar búsquedas por nombre.
+
+db.productos.createIndex({nombre:-1});
+
+
+// 2. Crear un índice compuesto sobre `categoria` y `precio` para facilitar búsquedas filtradas por ambas condiciones.
+
+db.productos.createIndex({categoria:-1,precio:1});
+
+// 3. Crear un índice en el campo `email` de `clientes` para validaciones rápidas de duplicados.
+
+db.clientes.createIndex({ email: 1 }, { unique: true });
+
+// 4. Usar `explain()` en una consulta para mostrar si el índice de `nombre` está siendo utilizado.
+
+db.productos.find({ nombre: "Borojó fresco" }).explain("executionStats");
+
